9f75977bda6995fc1a03c35ea035df74e336b386
Alexandr25041973 <sashan25041973@yandex.ru>
2023-08-03 04:42:13 +0300

    Add HomeWork task_064_066_068

---
+++ HW_task066/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+ элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int PrintNumberMN(int start, int end) //это и есть метод !! рекурсии
+{
+  if (start == end)
+    return start;//переменную start (int) и вывод(!!рекурсия завершена)
+  return (start + PrintNumberMN(start + 1, end));//!!!особенно этот момент нужно понимать
+}
+
+Clear();
+Write("Введите значение(m): ");
+int m = int.Parse(ReadLine());
+Write("Введите значение(n): ");
+int n = int.Parse(ReadLine());
+
+int sum = 0; 
+sum += PrintNumberMN(m, n);
+WriteLine($"M = {m}; N = {n} -> " + sum);
+
+
